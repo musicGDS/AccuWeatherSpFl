6361e18f43a01bb8c21fcd38f9bebf6be9222933
Gediminas Sudnikavicius <musicgds@gmail.com>
2020-08-26 20:49:16 +0300

    updated key parameter

--- AccuWeatherSpFl/Actions/ApiActions.cs
+++ AccuWeatherSpFl/Actions/ApiActions.cs
@@ -18,6 +18,7 @@ namespace AccuWeatherSpFl.Actions
         public ApiActions()
         {
             _client = new RestClient(BaseUrl);
+            //_client.Authenticator = new RestSharp.Authenticators.HttpBasicAuthenticator("key", key);
         }
 
         public List<T> Execute<T>(RestRequest request) where T : new()
@@ -36,11 +37,12 @@ namespace AccuWeatherSpFl.Actions
 
         public List<CityModel> GetCities(string countryCode)
         {
-            var request = new RestRequest("locations/v1/adminareas/{countryCode}?apikey=AdeGoEqdDkyxoT7gKAuRkAjIO4PG7aK5");
+            var request = new RestRequest("locations/v1/adminareas/{countryCode}");
             //request.RootElement = "Call";
             request.AddParameter("countryCode", countryCode, ParameterType.UrlSegment);
+            request.AddParameter("apikey", key);
             //request.AddParameter("key", key, ParameterType.UrlSegment);
-            
+
             return Execute<CityModel>(request);
         }
     }
